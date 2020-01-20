ceebb50e0ad2eb425adbfb114fa1e55e1799ab42
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-01-19 22:18:12 -0400

    Diseño Listo

---
+++ GeneroGenerator/Data/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using GeneroGenerator.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneroGenerator.Data
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<Generos> Genero { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server = (local); Database= Test1_Db; Trusted_Connection = True; ");
+
+        }
+    }
+}
---
+++ GeneroGenerator/Entidades/Generos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneroGenerator.Entidades
+{
+    public class Generos
+    {
+        [Key]
+        public int GeneroId { get; set; }
+        public string Nombre { get; set; }
+
+        public Generos()
+        {
+            GeneroId = 0;
+            Nombre = string.Empty;
+
+        }
+
+    }
+}
