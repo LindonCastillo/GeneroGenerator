0b3eca22634ce46b9db2646db3e7bf599e521625
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-01-19 23:28:38 -0400

    Diseño complete

---
+++ GeneroGenerator/BLL/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GeneroGenerator.BLL
+{
+    interface IRepository
+    {
+        public interface IRepository<T> where T : class
+        {
+            List<T> GetList(Expression<Func<T, bool>> Expression);
+            T Buscar(int id);
+            bool Guardar(T entity);
+            bool Modificar(T entity);
+            bool Eliminar(int id);
+        }
+    }
+}
