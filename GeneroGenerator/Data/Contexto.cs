f236ba908cffbe69beacf442814e2164fa142c34
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-01-19 23:49:13 -0400

    Ya Guarda

--- GeneroGenerator/Data/Contexto.cs
+++ GeneroGenerator/Data/Contexto.cs
@@ -10,11 +10,11 @@ namespace GeneroGenerator.Data
     public class Contexto : DbContext
     {
 
-        public DbSet<Generos> Genero { get; set; }
+        public DbSet<Generos> Generos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(@"Server = (local); Database= Test1_Db; Trusted_Connection = True; ");
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database= Test1_Db; Trusted_Connection = True; ");
 
         }
     }
